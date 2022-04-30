d911dd9bbadcf2ee1eda2fcdd6ceafcf030bc3d4
BARIŞ KOÇYİĞİT <l5218041@std.yildiz.edu.tr>
2022-04-30 18:50:03 +0300

    MovieApp

---
+++ src/app/movie.ts
@@ -0,0 +1,6 @@
+export class Movie{
+    id:any;
+    name:any;
+    description:any;
+    imageUrl:any;
+}
---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,45 @@
+import { Component } from "@angular/core";
+import { Movie } from "../movie";
+import { MovieService } from "../movie.service";
+
+
+@Component({
+    selector: 'movies', //<movies></movies>
+    templateUrl: 'movies.component.html'
+
+})
+
+export class MoviesComponent {
+    title = 'Movie List';
+    movies!: Movie[];
+    selectedMovie!: Movie;
+
+    constructor(private movieService: MovieService) { }
+
+    ngOnInit(): void {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+        this.getMovies();
+    }
+
+    onSelect(movie: Movie): void {
+        this.selectedMovie = movie;
+    }
+
+    getMovies(): void {
+        this.movieService.getMovies().subscribe(movies => this.movies = movies);
+    }
+
+    add(name:string,imageUrl:string,description:string):void{
+        this.movieService.add({
+            name,
+            imageUrl,
+            description
+        }as Movie).subscribe(movie=>this.movies.push(movie));
+    }
+
+    delete(movie:Movie):void{
+        this.movies = this.movies.filter(m=>m!==movie);
+        this.movieService.delete(movie).subscribe();
+    }
+}
